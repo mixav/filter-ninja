4da77e099891f1483e342faf742c5d42f6a64e5e
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2021-05-17 22:53:02 +0300

    load list with firebase

--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-// import * as firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/firestore"
+import firebase from "firebase/app";
+// import "firebase/analytics";
+import "firebase/database"
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDJ6op13XjY_1UpIo7WYBnagztBHDkxP8k",
-//     authDomain: "ninja-filter.firebaseapp.com",
-//     databaseURL: "https://ninja-filter.firebaseio.com",
-//     projectId: "ninja-filter",
-//     storageBucket: "ninja-filter.appspot.com",
-//     // messagingSenderId: "90019705706",
-//     // appId: "1:90019705706:web:f7b975857695f7f0456b13",
-//     measurementId: "G-53K86PRQ5R"
-// };
-//
-// firebase.initializeApp(firebaseConfig);
-// firebase.database();
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ6op13XjY_1UpIo7WYBnagztBHDkxP8k",
+    authDomain: "ninja-filter.firebaseapp.com",
+    databaseURL: "https://ninja-filter.firebaseio.com",
+    projectId: "ninja-filter",
+    storageBucket: "ninja-filter.appspot.com",
+    messagingSenderId: "90019705706",
+    appId: "1:90019705706:web:f7b975857695f7f0456b13",
+    measurementId: "G-53K86PRQ5R"
+};
+
+firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
+const ninjasRef = firebase.database().ref('ninjas');
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-// Vue.use(firebase)
+Vue.use(firebase)
 
+export {
+    ninjasRef
+}
 new Vue({
     render: h => h(App),
 }).$mount('#app')
